4b3db04ec3dcad0fe5e953ddf27ced84d5f3a429
zxacc.tt <ababsazaki85@gmail.com>
2026-04-22 08:40:22 +0000

    Update config.sh

--- config/config.sh
+++ config/config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Shared configuration for all modules
 
-REPORT_DIR="$HOME/NSCS_Audit_project/reports"
-LOG_DIR="$HOME/NSCS_Audit_project/logs"
-SCRIPTS_DIR="$HOME/NSCS_Audit_project/modules"
+REPORT_DIR="$HOME/Audit_project/reports"
+LOG_DIR="$HOME/Audit_project/logs"
+SCRIPTS_DIR="$HOME/Audit_project/modules"
